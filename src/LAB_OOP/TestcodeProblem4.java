a00a78d176829f6c846cf3b81a3f7db6bce3fdd9
ACER <cc7820197@gmail.com>
2026-03-27 08:42:12 +0700

    27/3

--- src/LAB_OOP/TestcodeProblem4.java
+++ src/LAB_OOP/TestcodeProblem4.java
@@ -6,9 +6,77 @@ public Person(String name, String address){
     this.name=name;
     this.address=address;
 }
-
-
-
+// Method
+public  String getName(){
+    return name;
+}
+public String getAddress(){
+    return address;
+}
+public void setAddress(String address){
+    this.address=address;
+}
+public String toString(){
+    return "Name:" +name+" "+" Address:"+address;
+}
+}
+class student extends Person{
+    private String program;
+    private int year;
+    private double fee;
+    public student(String name, String address, String program, int year, double fee){
+        super(name,address);
+        this.program=program;
+        this.year=year;
+        this.fee=fee;
+    }
+    public String getProgram(){
+        return program;
+    }
+    public int getYear(){
+        return year;
+    }
+    public double getFee(){
+        return fee;
+    }
+    public void setProgram(String program){
+        this.program=program;
+    }
+    public void setYear(int year){
+        this.year=year;
+    }
+    public void setFee(double fee){
+        this.fee=fee;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+" Program:"+program+" Year:"+year+" Fee:"+fee;
+    }
+}
+class staff extends Person{
+    private String school;
+    private double pay;
+    public staff(String name, String address, String school, double pay){
+        super(name,address);
+        this.school=school;
+        this.pay=pay;
+    }
+    public String getSchool(){
+        return school;
+    }
+    public double getPay(){
+        return pay;
+    }
+    public void setSchool(String school){
+        this.school=school;
+    }
+    public void setPay(double pay){
+        this.pay=pay;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+" School:"+school+" Pay:"+pay;
+    }
 }
 public class TestcodeProblem4 {
     public static void main(String[] args){
